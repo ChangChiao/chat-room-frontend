2076d4bc403ea01f9757f57bae8558fe61bbb888
JoeCT_Chang <JoeCT.Chang@moxa.com>
2024-09-18 14:51:15 +0800

    refactor: remove webpack config

--- src/app/page/auth-page/auth-page.component.ts
+++ src/app/page/auth-page/auth-page.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environment';
 
 @Component({
   selector: 'app-auth-page',
@@ -10,8 +11,13 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './auth-page.component.html',
   styleUrl: './auth-page.component.css',
 })
-export class AuthPageComponent {
+export class AuthPageComponent implements OnInit {
   http = inject(HttpClient);
+
+  ngOnInit() {
+    console.log('url', environment.apiPath);
+  }
+
   loginByGoogle() {
     this.http.get<any>('/api/v1/google').subscribe((config) => {
       // process the configuration.
--- src/app/services/http-service.ts
+++ src/app/services/http-service.ts
@@ -1,11 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { catchError, Observable, throwError } from 'rxjs';
+import { environment } from '../../../environment';
 
 @Injectable({ providedIn: 'root' })
 export class HttpService {
   #http = inject(HttpClient);
-  #basePath = process.env['API_URL'];
+  #basePath = environment.apiPath;
 
   get<T>(url: string): Observable<T> {
     return this.#http.get<T>(`${this.#basePath}${url}`).pipe(;
